ef10d31a1be21e949546e84da02e98a3ea65f65b
rendisagala <rendisagala19@gmail.com>
2022-08-05 22:13:28 +0700

    user models

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,43 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+  img: {
+    public_id: String,
+    url: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    uniqure: [true, "Email already exist"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minLength: [true, "Password must be at least 8 characters"],
+    select: false,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
 module.exports = mongoose.model("User", userSchema);
