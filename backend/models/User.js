aa7d396f8eda8411f8633370b09015da598266e8
rendisagala <rendisagala19@gmail.com>
2022-08-05 23:05:44 +0700

    generate token user models

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -49,6 +52,10 @@ userSchema.pre("save", async (next) => {
 
 userSchema.methods.matchPassword = async (password) => {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = () => {
+  return jwt.sign({ _id: this._id }, process.env.JWT);
 };
 
 module.exports = mongoose.model("User", userSchema);
