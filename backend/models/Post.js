de8e84b0108170f5a83961df6f96c566af5895f8
rendisagala <rendisagala19@gmail.com>
2022-08-05 22:08:30 +0700

    post models

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,5 +1,35 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({});
+const postSchema = new mongoose.Schema({
+  caption: String,
+  image: {
+    public_id: String,
+    url: String,
+  },
+  Author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  CreatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comments: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
 
 module.exports = mongoose.model("Post", postSchema);
