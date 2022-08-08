ae7a468494adfbb59f6ed68f91c98fb21118c4f4
rendisagala <rendisagala19@gmail.com>
2022-08-08 12:12:50 +0700

    search user by id

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -105,7 +105,22 @@ exports.logout = [
   },
 ];
 
-exports.user = [
+exports.userById = [
+  async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user)
+        return res
+          .status(404)
+          .json({ success: false, message: "User Not Found" });
+      return res.status(200).json({ success: true, user });
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error.message });
+    }
+  },
+];
+
+exports.myUser = [
   async (req, res) => {
     try {
       const user = await User.findById(req.user._id);
