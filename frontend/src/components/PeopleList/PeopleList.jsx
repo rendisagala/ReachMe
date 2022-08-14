33bcac33067c01c521bd737f6382518aab698abf
rendisagala <rendisagala19@gmail.com>
2022-08-14 12:42:23 +0700

    update people list

--- frontend/src/components/PeopleList/PeopleList.jsx
+++ frontend/src/components/PeopleList/PeopleList.jsx
@@ -9,34 +9,36 @@ export default function PeopleList() {
     dispatch(getAllUser());
   }, [dispatch]);
 
-  const { users: user } = useSelector((state) => state.allUser);
+  const { users } = useSelector((state) => state.allUser);
+  const { user } = useSelector((state) => state.user);
   return (
     <>
       <nav id="sidebar" className="col-2 w-25">
         <div className="pt-5">
           <h5>Check Who is here</h5>{" "}
-          {user?.map((data, index) => {
-            return (
-              <div
-                className="d-flex flex-row justify-content-between align-items-center p-2 border-bottom mb-2"
-                key={index}
-              >
-                <div className="d-flex flex-row align-items-center feed-text px-2">
-                  <img
-                    className="rounded-circle"
-                    src="https://i.imgur.com/aoKusnD.jpg"
-                    width="45"
-                  />
-                  <div className="d-flex flex-column flex-wrap ml-2">
-                    <span className="font-weight-bold  p-1">{data.name}</span>
-                    <span className="text-black-50 time">40 minutes ago</span>
+          {users?.map((data, index) => {
+            if (data._id.toString() !== user._id.toString())
+              return (
+                <div
+                  className="d-flex flex-row justify-content-between align-items-center p-2 border-bottom mb-2 row-cols-6"
+                  key={index}
+                >
+                  <div className="d-flex flex-row align-items-center feed-text px-2">
+                    <img className="rounded-circle" src={data.img} width="45" />
+                    <div className="d-flex flex-column flex-wrap ml-2">
+                      <span className="font-weight-bold  p-1">{data.name}</span>
+                      <span className="text-black-50 time p-1">
+                        {data.followers.length} Followers
+                      </span>
+                    </div>
+                  </div>
+                  <div className="float-start px-2">
+                    <button type="button" className="btn btn-outline-dark">
+                      Follow
+                    </button>
                   </div>
                 </div>
-                <div className="feed-icon px-2">
-                  <i className="fa fa-ellipsis-v text-black-50"></i>
-                </div>
-              </div>
-            );
+              );
           })}
         </div>
       </nav>
