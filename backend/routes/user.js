b2ed2575c9f9957c5e36ff38d64ba7467ae55233
rendisagala <rendisagala19@gmail.com>
2022-08-16 13:52:34 +0700

    update routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,7 +12,7 @@ router.route("/delete").delete(auth.isAuth, user.deleteUser);
 router.route("/user").get(auth.isAuth, user.myUser);
 router.route("/user/:id").get(auth.isAuth, user.userById);
 router.route("/users").get(auth.isAuth, user.allUser);
-router.route("/follow/:id").put(auth.isAuth, user.followAndUnfollow);
+router.route("/follow/:id").post(auth.isAuth, user.followAndUnfollow);
 router.route("/update/password").put(auth.isAuth, user.updatePassword);
 router.route("/update/user").put(auth.isAuth, user.updateUser);
 
