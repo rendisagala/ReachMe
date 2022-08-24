036140f738a5cf4dfcea944775046e2493825610
rendisagala <rendisagala19@gmail.com>
2022-08-24 19:55:24 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
