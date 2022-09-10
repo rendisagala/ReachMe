89e0eff31151998061d454cf0c3827a90de12320
rendisagala <rendisagala19@gmail.com>
2022-09-10 19:49:07 +0700

    samesite

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -84,6 +84,7 @@ exports.login = [
       const options = {
         expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        sameSite: "none",
       };
 
       return res
